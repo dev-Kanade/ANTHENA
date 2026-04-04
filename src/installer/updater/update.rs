477e663e755e073a361c97dcd98df8c98878c3a8
Kanade <kanade.miyachi@gmail.com>
2026-04-04 21:44:29 +0900

    d

--- src/installer/updater/update.rs
+++ src/installer/updater/update.rs
@@ -1,7 +1,11 @@
 pub fn update(){
     println!("更新データを確認中...");
+    let mut update = cheak_update();
+    println!("{update}");
 }
 
-fn cheak_update()->bool{
-    true
+fn cheak_update()->&str{
+    let update_size:&str = 12Mb;
+    let update_version:&str = 1.0;
+    "更新データが見つかりました。\nアップデートサイズ:{update_size}\nバージョン:{update_version}"
 }
