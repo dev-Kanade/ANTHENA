f4337dae784c353c6181bac4c0bd3169261a0bc9
Kanade <kanade.miyachi@gmail.com>
2026-04-04 20:27:58 +0900

    fix

--- src/installer/main.rs
+++ src/installer/main.rs
@@ -8,9 +8,9 @@ pub fn installer(installertype:i32){
     if installertype == 0 {
         installer::installer();
     }else if installertype == 1 {
-        updater::update::update::update();
+        updater::update::update();
     }else if installertype == 2 {
-        uninstaller::uninstall::uninstall::uninstall();
+        uninstaller::uninstall::uninstall();
     }else{
         eprintln!("インストーラーの起動に失敗しました。");
     }
