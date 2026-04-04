7b7bbf3f2c9015894dcbfc644f5f771d4b47118e
Kanade <kanade.miyachi@gmail.com>
2026-04-04 21:46:34 +0900

    ｓ

--- src/installer/updater/update.rs
+++ src/installer/updater/update.rs
@@ -1,6 +1,6 @@
 pub fn update(){
     println!("更新データを確認中...");
-    let mut update = cheak_update();
+    let update = cheak_update();
     if update == true {
         println!("更新データが見つかりました。\nアップデートしますか？(y/n)");
     }else{
