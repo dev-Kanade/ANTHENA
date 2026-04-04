ff24c9bc901a13521936740ca9b1d63a88140ada
Kanade <kanade.miyachi@gmail.com>
2026-04-04 20:14:24 +0900

    s

--- src/installer/main.rs
+++ src/installer/main.rs
@@ -5,6 +5,16 @@ mod updater;
 pub fn installer(installertype:i32){
     println!("Hello World!");
 
+    if installertype == 0 {
+        installer::installer();
+    }else if installertype == 1 {
+        updater::update();
+    }else if installertype == 2 {
+        uninstaller::uninstall();
+    }else{
+        eprintln!("インストーラーの起動に失敗しました。");
+    }
+
 }
 
 /* installertypeが0ならインストール,1ならアップデート,2ならアンインストールパッケージを実行します。*/
