52a5ce15f7a11de20a3a043c56087b39ffd8c70d
Kanade <kanade.miyachi@gmail.com>
2026-04-05 11:23:14 +0900

    エラー

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 mod installer;
 fn main() {
+    let cheak_system = cheak_anthena_system();
+    if cheak_system == true {
+        println!("[MEM]インストール済み");
+    }else if cheak_system == false {
+        installer::main::installer(0);
+    }else{
+        println!("ANTHENA起動中にエラーが発生しました。");
+    }
     installer::main::installer(0);
+}
+
+fn cheak_anthena_system()-> bool {
+
 }
