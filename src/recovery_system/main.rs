977ac11c7b044a6c26500cbe5d8f3eb36e12184a
奏 <kanade.miyachi@gmail.com>
2026-04-07 05:22:11 +0900

    aa

--- src/recovery_system/main.rs
+++ src/recovery_system/main.rs
@@ -2,3 +2,14 @@ pub fn recovery(){
     //ここに壊れているファイルを検知するコードを書きます。
 }
 
+fn recovery_table(){
+    //テーブルの修復
+}
+
+fn recovery_config(){
+    //構成ファイルの修復
+}
+
+fn recovery_user(){
+    //システムユーザーの修復
+}
