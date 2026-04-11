fdc027c418a2bf9109907f6480addc609366fef6
Kanade <you@example.com>
2026-04-11 03:51:16 +0000

    警告文

--- src/installer/installer.rs
+++ src/installer/installer.rs
@@ -15,12 +15,12 @@ fn printwelcom(){
     ║                                   ║
     ║          A N T H E N A            ║
     ║                                   ║    
-    ║        認証・認可システム           ║
+    ║        認証・認可システム         ║
     ║                                   ║
     ║     Authentication & Auth.        ║
     ║                                   ║
     ╚═══════════════════════════════════╝
-    "#;
+    "#;//絶対に手を加えないで
 
     println!("{logo}");
     println!("Welcom to ANTHENA Instaler!!");
