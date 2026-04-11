6724a290a08d6a2cc59898e17fc8ea5e69a7e8ee
Kanade <you@example.com>
2026-04-11 06:39:57 +0000

    API関連の関数をすべて削除位

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,11 @@ use std::env;
 fn main() {
     let cheak_system = cheak_anthena_system();
     if cheak_system == true {
-        api_server();
     }else if cheak_system == false {
         installer::main::installer(0);//これは、将来的にif文に直してください。
     }else{
         println!("[INF]ANTHENA起動中にエラーが発生しました。");
     }
-    api_server();
 }
 
 fn cheak_anthena_system()-> bool {
