be433d176f42e2f53f83a666bd43c0f80dff686b
奏 <kanade.miyachi@gmail.com>
2026-04-15 19:24:21 +0900

    構文エラーを修正

--- src/installer/main.rs
+++ src/installer/main.rs
@@ -4,12 +4,12 @@ pub fn installer(installertype:i32){
     let _ = Command::new("sudo")
         .args(["-l"])
         .output();
-        
+
     match installertype{
-        0 => super::installer::installer();
-        1 => super::updater::update::update();
-        2 => super::uninstaller::uninstall::uninstall();
-        _ => println!("インストーラーの起動に失敗しました。");
+        0 => super::installer::installer(),
+        1 => super::updater::update::update(),
+        2 => super::uninstaller::uninstall::uninstall(),
+        _ => println!("インストーラーの起動に失敗しました。"),
     }
 }
 
